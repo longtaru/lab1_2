61b190b04a4166068f293ae33e10a8207e1e621b
longtaru <longhlvt123@gmail.com>
2025-11-14 10:26:02 +0700

    tạo đối tượng

---
+++ app/src/main/java/fpoly/longnd/lab12/dto/CatDTO.java
@@ -0,0 +1,22 @@
+package fpoly.longnd.lab12.dto;
+
+public class CatDTO {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/fpoly/longnd/lab12/dto/ProductDTO.java
@@ -0,0 +1,40 @@
+package fpoly.longnd.lab12.dto;
+
+public class ProductDTO {
+    int id;
+    String name;
+    double price;
+    int id_cat;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getId_cat() {
+        return id_cat;
+    }
+
+    public void setId_cat(int id_cat) {
+        this.id_cat = id_cat;
+    }
+}
